ae193534891321645dcdd7e3a31b7d59a809c3b6
mjdubell <mjdubell@users.noreply.github.com>
2019-04-13 23:43:52 +0200

    Closes #11 - Try to upgrade image links to HTTPS

---
+++ utils/tests/test_web.py
@@ -0,0 +1,26 @@
+""" Test web Utils """
+
+from unittest import mock
+
+from django.test import SimpleTestCase
+
+from utils.web import https_upgrade
+
+
+class WebUtilsTestCase(SimpleTestCase):
+    """ Tests """
+
+    @mock.patch("utils.web.requests.get")
+    def test_https_upgrade(self, mock_requests):
+        """ Test """
+        mock_requests.return_value.status_code = 200
+
+        http = "http://dubell.io"
+        https = "https://dubell.io"
+        domain = "dubell.io"
+        www = "www.dubell.io"
+
+        self.assertEqual(https_upgrade(http), https)
+        self.assertEqual(https_upgrade(https), https)
+        self.assertEqual(https_upgrade(domain), https)
+        self.assertEqual(https_upgrade(www), f"https://{www}")
